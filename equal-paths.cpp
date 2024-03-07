ba9d1c7435fb70ec917ee6bc46a24640858257b9
Adrian Thomas <adriancleve1@gmail.com>
2024-03-06 17:55:39 -0800

    equal paths implementation

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <algorithm>
 
 #endif
 
@@ -8,11 +9,22 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int getHeight(Node *node) {
+	if (node == nullptr)
+		return 0;
 
+	return 1 + max(getHeight(node->left), getHeight(node->right));
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if (root == nullptr)
+		return true;
+
+	if (getHeight(root->left) - getHeight(root->right) != 0) return false;
+
+	return true;
 
 }
 
